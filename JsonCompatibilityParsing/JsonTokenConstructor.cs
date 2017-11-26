319ef15b683e01d464c040c3f2062edc61e79a10
Daniel Morrison <dan@morrison.ph>
2017-11-26 14:08:33 -0500

    Make JToken constructor selection more readable

--- JsonCompatibilityParsing/JsonTokenConstructor.cs
+++ JsonCompatibilityParsing/JsonTokenConstructor.cs
@@ -17,38 +17,39 @@ namespace JsonCompatibilityParsing
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
+            var token = JToken.ReadFrom(reader);
             var allConstructors = objectType.GetConstructors();
             var constructors = allConstructors
                 .Where(c => c.CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(JsonConstructorAttribute)) != null)
                 .ToArray();
-            if (constructors.Length > 1) throw new Exception("Ambiguous JsonConstructorAttribute");
-            if (constructors.Length == 0)
-            {
-                constructors = allConstructors
-                    .Where(c =>
-                    {
-                        var args = c.GetParameters();
-                        if (args.Length < 1) return false;
-                        if (args.Length > 2) return false;
-                        if (!typeof(JToken).IsAssignableFrom(args[0].ParameterType)) return false;
-                        if (args.Length >= 2)
-                        {
-                            if (args[1].ParameterType != typeof(object)) return false;
-                        }
-                        return true;
-                    })
-                    .ToArray();
+            if (constructors.Length == 0) constructors = allConstructors;
+
+            var constructorInfo = constructors
+                .Select(c => new
+                {
+                    Constructor = c,
+                    Parameters = c.GetParameters().ToArray(),
+                })
+                // Ignore constructors with more than two parameters
+                .Where(ci => ci.Parameters.Length > 0 && ci.Parameters.Length <= 2)
+                // Ignore constructors where the first argument isn't a JToken
+                .Where(ci => typeof(JToken).IsAssignableFrom(ci.Parameters.FirstOrDefault()?.ParameterType))
+                // Ignore constructors where the second argument isn't an object
+                .Where(ci => new[] { null, typeof(object) }.Contains(ci.Parameters.Skip(1).FirstOrDefault()?.ParameterType))
+                .ToArray();
+            
+            if (constructorInfo.Length > 1) throw new Exception("Ambiguous JToken constructor");
+            if (constructorInfo.Length == 0) {
+                throw new Exception($"No JToken constructor found {objectType.Name}(JToken token[, object existingValue = null])");
             }
-            if (constructors.Length > 1) throw new Exception("Ambiguous JToken constructor");
-            if (constructors.Length == 0) throw new Exception($"No JToken constructor found {objectType.Name}(JToken token[, object existingValue = null])");
-            var constructor = constructors[0];
-            var parameters = constructor.GetParameters().ToArray();
-            var token = JToken.ReadFrom(reader).ToObject(parameters[0].ParameterType);
+            var constructor = constructorInfo[0].Constructor;
+            var parameters = constructorInfo[0].Parameters;
+            var castToken = token.ToObject(parameters[0].ParameterType);
             if (parameters.Length == 2)
             {
-                return constructor.Invoke(new object[] { token, existingValue });
+                return constructor.Invoke(new object[] { castToken, existingValue });
             }
-            return constructor.Invoke(new object[] { token });
+            return constructor.Invoke(new object[] { castToken });
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
